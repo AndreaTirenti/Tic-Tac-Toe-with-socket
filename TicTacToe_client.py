fc2286dd569e6f77c3945f397a84f20adb65cc12
Andrea Tirenti <115592242+AndreaTirenti@users.noreply.github.com>
2022-10-14 12:53:04 +0200

    Update TicTacToe_client.py

--- TicTacToe_client.py
+++ TicTacToe_client.py
@@ -1,6 +1,6 @@
 import socket      
 import sys
-from tris import Tris
+from TicTacToe import TicTacToe
 
 host = "127.0.0.1"
 port = 5001
@@ -28,7 +28,7 @@ board = {
 PLAYER1 = "X"
 PLAYER2 = "O"
 
-tris = Tris(PLAYER1, PLAYER2, board)
+TicTacToe = TicTacToe(PLAYER1, PLAYER2, board)
 
 
 print("TRIS")
@@ -37,19 +37,19 @@ print("Client is X and Server is O")
 turn = 0
 
 while 1:
-    if tris.Check_Win(board) != 0:
+    if TicTacToe.Check_Win(board) != 0:
         my_sock.send("break".encode())
         break
 
     if turn % 2 == 0:
         print("Client make your move according this scheme:\n1 | 2 | 3\n--+---+--\n4 | 5 | 6\n--+---+--\n7 | 8 | 9\n")
-        print(tris.Print_Board(board)) 
-        tris.Player_Move(board, PLAYER1)
-        if tris.Check_Win(board) != 0:
+        print(TicTacToe.Print_Board(board)) 
+        TicTacToe.Player_Move(board, PLAYER1)
+        if TicTacToe.Check_Win(board) != 0:
             my_sock.send("break".encode())
             break
         else:
-            my_sock.send(tris.Print_Board(board).encode())
+            my_sock.send(TicTacToe.Print_Board(board).encode())
     else:
         move = my_sock.recv(256)
         if board[int(move.decode())] != "_":
@@ -59,17 +59,17 @@ while 1:
 
     turn+=1
 
-result = tris.Check_Win(board)
+result = TicTacToe.Check_Win(board)
 if result == 0:
-    print(tris.Print_Board(board))
+    print(TicTacToe.Print_Board(board))
     print("DRAW!")
     my_sock.send("DRAW!".encode())
 elif result == 10:
-    print(tris.Print_Board(board))
+    print(TicTacToe.Print_Board(board))
     print("CLIENT WINS!")
     my_sock.send("CLIENT WINS!".encode())
 elif result == -10:
-    print(tris.Print_Board(board))
+    print(TicTacToe.Print_Board(board))
     print("SERVER WINS!")
     my_sock.send("SERVER WINS!".encode())
 
