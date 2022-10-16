bb8ab64abdba2ebf824cac18c62b8e66c1a5af89
Andrea Tirenti <115592242+AndreaTirenti@users.noreply.github.com>
2022-10-16 21:16:16 +0200

    Update TicTacToe_server.py

--- TicTacToe_server.py
+++ TicTacToe_server.py
@@ -18,21 +18,59 @@ conn, addr = my_sock.accept()
 print ('Incoming connection from', addr)
 
 
+board = {
+        1 : "_", 2 : "_" , 3 : "_" ,
+        4 : "_", 5 : "_" , 6 : "_" ,
+        7 : "_", 8 : "_" , 9 : "_" ,
+        }
+
+PLAYER1 = "X"
+PLAYER2 = "O"
+
+tris = TicTacToe(PLAYER1, PLAYER2, board)
+
+turn = 0
+
 print("TRIS")
-print("Client is X and Server is O")
+print("Client is O and Server is X")
 
 while 1:
-    board = conn.recv(256)
-    if board.decode() == "break":
-        print("break\n")
+    if tris.Check_Win(board) != 0:
+        conn.send("break".encode())
         break
-    print("\nServer make your move according this scheme:\n1 | 2 | 3\n--+---+--\n4 | 5 | 6\n--+---+--\n7 | 8 | 9\n")
-    print(board.decode())
-    move = input("Make your move-> ")
-    conn.send(move.encode())
+
+    if turn % 2 == 0:
+        print("Server make your move according this scheme:\n1 | 2 | 3\n--+---+--\n4 | 5 | 6\n--+---+--\n7 | 8 | 9\n")
+        print(tris.Print_Board(board)) 
+        tris.Player_Move(board, PLAYER1)
+        if tris.Check_Win(board) != 0:
+            conn.send("break".encode())
+            break
+        else:
+            conn.send(tris.Print_Board(board).encode())
+    else:
+        move = conn.recv(256)
+        if board[int(move.decode())] != "_":
+            print("WRONG MOVE!")
+            exit(0)
+        board[int(move.decode())] = PLAYER2
+
+    turn+=1
     
-result = conn.recv(256)
-print(result.decode())
+result = tris.Check_Win(board)
+if result == 0:
+    print(tris.Print_Board(board))
+    print("DRAW!")
+    conn.send("DRAW!".encode())
+elif result == 10:
+    print(tris.Print_Board(board))
+    print("SERVER WINS!")
+    conn.send("SERVER WINS!".encode())
+elif result == -10:
+    print(tris.Print_Board(board))
+    print("CLIENT WINS!")
+    conn.send("CLIENT WINS!".encode())
+    
 
 
 conn.close()
